80a13d405e5cfd6f68d8c2bbb71e725fe015fb9a
Ragul Prasath <118117902+ragul-prasath@users.noreply.github.com>
2025-09-08 12:33:06 +0530

    Update dimension.py

--- dimension.py
+++ dimension.py
@@ -1,4 +1,4 @@
-'''check dimension of array'''
+'''check dimension of array, datatype, shape, size '''
 import numpy as np
 
 a = np.array(42)
@@ -7,6 +7,6 @@ c = np.array([[1, 2, 3], [4, 5, 6]])
 d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
 print(a.ndim)
-print(b.ndim)
-print(c.ndim)
-print(d.ndim)
+print(b.ndtype)
+print(c.shape)
+print(d.size)
